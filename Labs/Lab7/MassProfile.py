946603cb5d84e7fe0aca625a152ff62ebcc906de
Emily Walla <ewalla02@email.arizona.edu>
2020-03-24 21:23:35 -0700

    Added Research Assignment folder

--- Labs/Lab7/MassProfile.py
+++ Labs/Lab7/MassProfile.py
@@ -1,186 +1,188 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
 
 
-# inputs
+# Homework 5 Solutions
+# Mass Profiles
+# G. Besla 
+
+
+
+# import modules
 import numpy as np
 import astropy.units as u
-import astropy.table as tbl
-import matplotlib.pyplot as plt
-from Readfile import Read
-from CenterOfMass import CenterOfMass as COM
 from astropy.constants import G
-# convert G to correct units
-G = G.to(u.kpc*u.km**2/u.s**2/u.Msun)
 
-# create class MassProfile
+# import plotting modules
+import matplotlib.pyplot as plt
+import matplotlib
+
+# my modules
+from ReadFile import Read
+from CenterOfMass import CenterOfMass
+
+
+# Gravitational Constant
+# converting G to units of kpc*km^2/s^2/Msun
+G = G.to(u.kpc*u.km**2/u.s**2/u.Msun) # 4.498768e-6*u.kpc**3/u.Gyr**2/u.Msun
+
+
 class MassProfile:
-    # Class to define the mass profile of a galaxy at a snapshot time
+    # Class to define the Mass and Rotation Curve of a Galaxy
     
     def __init__(self, galaxy, snap):
-        
-        # inputs:
-        #       galaxy: a string with galaxy name, such as "MW" or "M31"
-        #       snap:  the snapshot number, such as 0, 1, etc
-        
-        # Initialize instance of this class with the following properties:
-        
-        # store the name of the galaxy
-        self.gname = galaxy
-        
+    # Initialize the instance of this Class with the following properties:
+    # galaxy :  string, e.g. "MW"
+    #  snap :  integer, e.g  1
+    
+        # Determine Filename
         # add a string of the filenumber to the value "000"
-        ilbl = "000"+str(snap)
-        
+        ilbl = '000' + str(snap)
         # remove all but the last 3 digits
         ilbl = ilbl[-3:]
-        self.filename = "%s_"%(galaxy)+ilbl+".txt"
-      
-        # read in the data of our desired file
-        self.time, self.total, self.data = Read(self.filename) 
+        # create filenames
+        self.filename='%s_'%(galaxy) + ilbl + '.txt'
+        
+        # read in the file                                                                                             
+        self.time, self.total, self.data = Read(self.filename)
+
+        # store the mass, positions, velocities of all particles                                
+        self.m = self.data['m']#*u.Msun
+        self.x = self.data['x']*u.kpc
+        self.y = self.data['y']*u.kpc
+        self.z = self.data['z']*u.kpc
     
-        # read in the data for the x, y, z positions and the mass
-        self.m = self.data["m"]
-        self.x = self.data["x"]*u.kpc
-        self.y = self.data["y"]*u.kpc
-        self.z = self.data["z"]*u.kpc
+        # store galaxy name
+        self.gname = galaxy
+    
+    
+    
+    def MassEnclosed(self, ptype, R):
+    # Function that determines the MassEnclosed of particles of a given type
+    # input: ptype  Particle type,  1=Halo, 2=Disk, 3=Bulge
+    #        R   An Array of Radii within which to compute the mass enclosed. 
+    # return: an array with the Mass enclosed (units of Msun)
         
-    def MassEnclosed(self, component, r):
-        # function that computes the mass enclosed
-        #      within a given radius of the COM position
-        #      for a specified galaxy & a specified component of galaxy
-        
-        
-        # inputs:
-        #      component: the component you want the mass of
-        #           either 1 (Halo), 2 (Disk) or 3 (Bulge)
-        #      r: the radii (kpc) in which you want to calculate the mass
-        #           input r as an array
-        
-        # returns:
-        #      an array of the mass enclosed (Msun) within a given radius
-        
-        
-        # Determine COM Position
-        CoM = COM(self.filename,component).COM_P(0.00001)
-        # distance to the center of mass
-        COM_D = np.sqrt(CoM[0]**2 + CoM[1]**2 + CoM[2]**2)
-        
-        # create an index to extract only the component we are using
-        index = np.where(self.data["type"]==component)
-        
-        # get mass, x, y z  of only the desired component
-        part_mass = self.m[index]
-        part_x = self.x[index].value - CoM[0]
-        part_y = self.y[index].value - CoM[1]
-        part_z = self.z[index].value - CoM[2]
-        
-        # calculate radii for our component
-        part_r = np.sqrt( (part_x**2)+(part_y**2)+(part_z**2) )
-        
-        # find Mass Enclosed within that radius of the COM
-        Mass = np.zeros_like(r)
-        
-        r_index = np.zeros_like(r)
-        
-        
-        i = 0
-        for i in range(len(r)):
-            # create index to get all points within given radius
-            #r_index[i] = np.where(part_r <= r[i])
-            Mass[i] = np.sum( part_mass[np.where( part_r < r[i])] )
+        # Determine the COM position using Disk Particles
+        # Disk Particles afford the best centroiding.
+        # Create a COM object
+        COM = CenterOfMass(self.filename,2)
+        # Store the COM position of the galaxy
+        # Set Delta = whatever you determined to be a good value in Homework 4.
+        GalCOMP = COM.COM_P(0.1)
             
-        return Mass*1e10*u.Msun
-    
-    
-    
-    def MassEnclosedTotal(self, r):
-        # function calculates the total mass enclosed within the given radii
-        
-        # inputs:
-        #      r: a 1D array of radii (kpc)
-        
-        # returns:
-        #      a 1D array of the total masses enclosed within the radii
-        
-        
-        # make a caveat for M33, galaxy with no bulge
-        if self.gname == "M33":
-            iterate = 2 # M33 only has 2 components
-        else:
-            iterate = 3 # The other galaxies have 3 components
-        
-        # make an array to store the masses in
-        part_mass = np.zeros((iterate,len(r)))
-        
-        # get the masses of each component
-        i = 0
-        for i in range(iterate):
-            part_mass[i,:] = self.MassEnclosed(i+1,r)
-      
-        return np.sum(part_mass,axis=0)
+        # create an array to store indexes of particles of desired Ptype                                                
+        index = np.where(self.data['type'] == ptype)
+
+        # Store positions of particles of given ptype from the COMP. 
+        xG = self.x[index] - GalCOMP[0]
+        yG = self.y[index] - GalCOMP[1]
+        zG = self.z[index] - GalCOMP[2]
             
-    def HernquistMass(self, r, a, Mhalo):
-        # function computes the mass enclosed within a given radius
-        #     given the theoretical Hernquist profile
-        
-        
-        # inputs:
-        #     r: 1D array of radii (kpc) from Center of mass position
-        #     a: scale factor
-        #     Mhalo: the halo mass of the galaxy (Msun)
-        
-        # returns:
-        #      Returns halo mass (Msun)
-        
-        return (Mhalo*(r**2))/((a+r)**2)
+        # Compute the mag. of the 3D radius
+        rG = np.sqrt(xG**2 + yG**2 + zG**2)
             
-        
-    def CircularVelocity(self, component, r):
-        # function that computes the circular velocity of the enclosed mass
-        # assumes spherical symmetry
-        # Vc = (GM/R)^1/2
-        
-        # inputs:
-        #     component: the type of particle you're working with
-        #           1 (halo), 2 (disk), 3 (bulge)
-        #     r: 1D array of radii (kpc)
-        
-        # returns:
-        #     1D array of circular speeds in units of km/s
-        
-        M = self.MassEnclosed(component, r)
-        
-        return np.sqrt(G*M/r)
-
-    def CircularVelocityTotal(self, r):
-        # function that calculates the circular velocity of the total enclosed mass
-        
-        # inputs: r: 1D array of radii (kpc)
-        
-        # returns: circular velocity of total enclosed mass (km/s)
-        
-        M = self.MassEnclosedTotal(r)
-        
-        return np.sqrt(G*M/r)
+        # store mass of particles of a given ptype
+        mG = self.m[index]
+            
+        # Array to store enclosed mass as a function of the input radius array
+        Menc = np.zeros(np.size(R))
     
-    def HernquistVCirc(self,r, a, Mhalo):
-        # computes the circular speed of the Hernquist mass
+        # set up a while loop that continues until the end of the input radius array
+        for i in range(np.size(R)):
+            # Only want particles within the given radius
+            indexR = np.where(rG < R[i]*u.kpc)
+            Menc[i] = np.sum(mG[indexR])*1e10         
         
-        # inputs:
-        #     r: 1D array of radii (kpc) from Center of mass position
-        #     a: scale factor
-        #     Mhalo: the halo mass of the galaxy (Msun)
+        # return the array of enclosed mass with appropriate units
+        return Menc*u.Msun
         
-        # returns: the circular speed of the Hernquist Mass (km/s)
     
-        M = self.HernquistMass(r,a,Mhalo)
-        return np.round((np.sqrt(G*M/r)),2)
-
-
-# In[ ]:
-
+    def MassEnclosedTotal(self, R):
+    # Determine the total mass of each galaxy point
+        # Input:   R   Radius 
+        # Returns: Mass in units Msun. 
+            
+        # Sum up all the mass of each component.
+        Menc = self.MassEnclosed(1,R) + self.MassEnclosed(2,R) + self.MassEnclosed(3,R)
+    
+        # Recall that M33 only has 2 components!  No bulge
+        if (self.gname == 'M33'):
+            Menc = self.MassEnclosed(1,R) + self.MassEnclosed(2,R)  
+          
+        return Menc
+    
+        
+        
+    def HernquistMass(self, R, scale, Mhalo):
+    # Determine the mass enclosed using Hernquist 1990 Mass profile 
+    # Input:   R   Radius  
+    #         scale   Scale Length  
+    #         Mhalo  Total Halo Mass (Msun)
+    # Returns: Mass in units Msun. 
+    
+        # Hernquist 1990 Mass profile
+        return Mhalo*R**2/(R+scale)**2*u.Msun
+       
+        
+        
+        
+    def CircularVelocity(self, ptype, R):
+    # Function that determines the circular speed at a given radius using the mass enclosed
+    # Input:  ptype  Particle type
+    #         R    ARRAY of Radii 
+    # Returns:  An ARRAY of Circular speeds in units of km/s
+    # NOTE: this method assumes spherical symmetry - but the disk is not spherically symmetric.
+    
+        # call MassEnclosed to determine array of masses within R.
+        Menc = self.MassEnclosed(ptype,R)
+        
+        # Determine the circular speed as a function of input radius assuming spherical symmetry 
+        # note that rad needs units 
+        # This will return units of kpc/Gyr
+        Vcirc = np.round(np.sqrt(G*Menc/R/u.kpc),2)
+        
+        # Return array Vcirc
+        return  Vcirc 
+    
+    
+    def CircularVelocityTotal(self, R):
+    # Function that determines the combined circular speed, using all galactic components
+    # Input:  R Array of Radii
+    # Returns: An Array of Circular speends in units of km/s
+    
+        # compute the total mass enclosed
+        Menc = self.MassEnclosedTotal(R)
+        
+        # Determine the circular speed as a function of input radius assuming spherical symmetry 
+        # note that rad needs units 
+        # This will return units of kpc/Gyr
+        Vcirc = np.round(np.sqrt(G*Menc/R/u.kpc),2)
+        
+        # Return array Vcirc
+        return  Vcirc 
+    
+    
+    
+    def HernquistVCirc(self, R, scale, Mhalo):
+    # Function that determines the theoretical circular speed of a Hernquist 1990 Halo
+    # input:  R  radius
+    #        scale  Scale Length  
+    #         Mhalo  Total Halo Mass (Msun)
+    # Returns: Vircular Velocity in units km/s.        
+             
+        # Store the enclosed mass 
+        Menc = self.HernquistMass(R,scale,Mhalo)
+    
+        # Circular speed using enclosed mass
+        # rounded to 2 decimal places 
+        # You could also write this analytically without first calling HernquistMass
+        Vcirc = np.round(np.sqrt(G*Menc/R/u.kpc),2)
+        
+        # Returns array Vcirc 
+        return Vcirc 
+    
+    
+        
+        
 
 
 
